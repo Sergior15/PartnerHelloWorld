1ae9d5461d92f0e89108a4a5537cd5bc463c09d3
Mark Palmer <mark.palmer@itsalllive.com>
2019-05-15 11:05:21 -0400

    changed message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace PartnerHello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello sergio!");
         }
     }
 }
