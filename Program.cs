1f9f0450d23b3577c9411952d6a193ee0f8fe1bc
Sergio Rodriguez <rsergio98@yahoo.com>
2019-05-15 10:12:12 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartnerHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
